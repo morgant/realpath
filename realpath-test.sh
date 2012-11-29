4ccdf96972db25c65ec89fcc5f952dda38442433
Morgan Aldridge <morgant@makkintosshu.com>
2012-11-29 07:31:22 -0500

    A couple more tests implemented in realpath-test.sh (specifically, testing bare root path and for failure if path contains tilde '~').

--- realpath-test.sh
+++ realpath-test.sh
@@ -22,14 +22,11 @@ it_converts_absolute_paths_without_change() {
 	test "$out_path" = "$rel_path"
 }
 
-it_converts_relative_home_paths() {
-	user="$(whoami)"
-	
-	rel_path="~/Desktop/"
-	abs_path="/Users/${user}/Desktop/"
+it_converts_root_path_without_change() {
+	rel_path="/"
 	out_path="$(./realpath "$rel_path")"
 	
-	test "$out_path" = "$abs_path"
+	test "$out_path" = "$rel_path"
 }
 
 it_converts_dotted_relative_paths() {
@@ -73,6 +70,16 @@ it_converts_local_paths_which_exist_in_root_path() {
 
 it_fails_for_nonexistent_paths() {
 	rel_path="/usr/local/mojo-jojo/but_first--i_must_attend_to_the_dishes_that_i_have_soiled_with_the_food_that_i_have_eaten"
+	out=$(set +e ; ./realpath "$rel_path" >/dev/null ; echo $?)
+	
+	test $out -gt 0
+}
+
+it_fails_for_relative_home_paths() {
+	user="$(whoami)"
+	
+	rel_path="~/Desktop/"
+	abs_path="/Users/${user}/Desktop/"
 	out=$(set +e ; ./realpath "$rel_path" >/dev/null ; echo $?)
 	
 	test $out -gt 0
